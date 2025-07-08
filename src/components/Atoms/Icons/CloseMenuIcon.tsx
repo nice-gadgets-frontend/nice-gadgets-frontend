dd05c68de88af3845c7ce76ea515a5aa8996d7b7
Yelyzaveta Zhyrova <lizzzhirova@gmail.com>
2025-07-08 12:53:09 +0300

    DEV-43 remove border on X Icon

--- src/components/Atoms/Icons/CloseMenuIcon.tsx
+++ src/components/Atoms/Icons/CloseMenuIcon.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 export const CloseIcon = () => {
   return (
-    <Link to="/home" className="inline-block border p-2 cursor-pointer">
+    <Link to="/home" className="inline-block p-2 cursor-pointer">
       <X
         size={16}
         color="#F1F2F9"
