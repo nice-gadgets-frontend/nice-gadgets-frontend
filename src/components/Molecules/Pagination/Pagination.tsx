1f30e19b5077c4b76d9ef6216dfe966cc6be99a1
murmurs <marchenkomeryam@gmail.com>
2025-07-08 19:24:31 +0300

    DEV-34 feat: pagination

---
+++ src/components/Molecules/Pagination/Pagination.tsx
@@ -0,0 +1,88 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import type React from "react";
+import { useSearchParams } from "react-router-dom";
+import { PaginationButton } from "../../Atoms/Buttons/PaginationButton";
+import cn from "classnames";
+
+
+type PaginationProps = {
+  totalItems: number;
+  perPage: number;
+};
+
+const DEFAULT_PAGE = 1;
+
+export const Pagination: React.FC<PaginationProps> = ({
+  totalItems,
+  perPage,
+}) => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageParam = Number(searchParams.get('page'));
+  const currentPage = pageParam >= 1 ? pageParam : DEFAULT_PAGE;
+
+  const totalPages = Math.ceil(totalItems / perPage);
+
+  const updatePage = (page: number) => {
+    const newParams = new URLSearchParams(searchParams);
+    
+    if (page === DEFAULT_PAGE) {
+      newParams.delete('page');
+    } else {
+      newParams.set('page', String(page));
+    }
+
+    setSearchParams(newParams);
+  }
+
+  if (totalPages <= 1) {
+    return null;
+  }
+
+  return (
+    <div className="flex justify-center items-center gap-2 py-4">
+      <button
+        onClick={() => updatePage(currentPage - 1)}
+        disabled={currentPage === 1}
+        className={cn('w-8 h-8 flex cursor-pointer justify-center items-center bg-surface-1 transition-colors',
+           {
+            'bg-surface-1 text-white hover:bg-accent': currentPage > 1,
+             'bg-black text-icons cursor-default opacity-100': currentPage === 1,
+           }
+  )}
+      >
+        <ChevronLeft
+          size={16}
+          className={currentPage === 1 ? 'text-icons' : 'text-white'}
+        />
+        </button>
+
+       <div className="flex mx-4 gap-2">
+        {[...Array(totalPages)].map((_, i) => {
+          const page = i + 1;
+          return (
+            <PaginationButton
+              key={page}
+              page={page}
+              selected={currentPage === page}
+              onClick={() => updatePage(page)}
+            />
+          )
+        })}
+        </div>
+      <button
+        onClick={() => updatePage(currentPage + 1)}
+        disabled={currentPage === totalPages}
+        className="w-8 h-8 cursor-pointer flex justify-center items-center bg-surface-1 text-white hover:bg-accent disabled:opacity-100"
+      >
+        <ChevronRight
+          size={16}
+          className={currentPage === totalPages ? 'text-icons' : 'text-white'}
+        />
+      </button>
+
+
+    </div>
+  )
+
+}
--- src/components/organisms/Pages/Catalog/Phone/PhonesCatalogPage.tsx
+++ src/components/organisms/Pages/Catalog/Phone/PhonesCatalogPage.tsx
@@ -1,10 +1,15 @@
 import {useState} from "react";
 
 import { CatalogFilters } from "../CatalogFilters/CatalogFilters";
+import { Pagination } from "../../../../Molecules/Pagination/Pagination";
+
 
 export const PhonesCatalogPage = () => {
   const [sortBy, setSortBy] = useState(new Set(["newest"]));
   const [itemsOnPage, setItemsOnPage] = useState(new Set(["16"]));
+
+   const selected = Array.from(itemsOnPage)[0]; // наприклад "16" або "all"
+  const perPage = selected === "all" ? 50 : Number(selected); // конвертуємо
 
   return (
     <div className="flex flex-col">
@@ -19,6 +24,9 @@ export const PhonesCatalogPage = () => {
         <h2 className="text-lg font-semibold text-gray-200">Tablets</h2>
         <p className="text-gray-400">Tablet items will be displayed here.</p>
       </div>
+      <Pagination totalItems={50} perPage={perPage} />
+      
     </div>
+   
   );
 };
