30fcb242bbd4bc2b32a0f140760196606c1c81a6
torry455 <kucerv8@gmail.com>
2025-07-10 20:37:01 +0300

    DEV-38: added main logic to product-detail-page

--- src/types/ProductType.ts
+++ src/types/ProductType.ts
@@ -1,19 +1,3 @@
-// export interface ProductType {
-//   id: number;
-//   category: string;
-//   itemId: string;
-//   name: string;
-//   fullPrice: number;
-//   price: number;
-//   screen: string;
-//   capacity: string;
-//   color: string;
-//   ram: string;
-//   year: number;
-//   image: string;
-// };
-
-
 export interface DescriptionSection {
   title: string;
   text: string[];
