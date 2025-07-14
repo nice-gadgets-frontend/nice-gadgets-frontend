76af559d48f3dc438298fcdf2e884fde66c8a3dc
Yelyzaveta Zhyrova <lizzzhirova@gmail.com>
2025-07-14 11:26:46 +0300

    DEV-50 update ShopByCategory section

--- src/components/Molecules/CategoryCard/CategoryCard.tsx
+++ src/components/Molecules/CategoryCard/CategoryCard.tsx
@@ -16,38 +16,43 @@ export const CategoryCard = ({
 
   switch (categoryType) {
     case 'Phones':
-      imgSrc = '/gadgets/img/category-phones-test.png';
+      imgSrc = '/gadgets/img/figma-category-phone.png';
       bgColor = '#6D6474';
       break;
     case 'Tablets':
-      imgSrc = '/gadgets/img/category-tablets-test.png';
+      imgSrc = '/gadgets/img/category-tablets.png';
       bgColor = '#8D8D92';
       break;
     case 'Accessories':
-      imgSrc = '/gadgets/img/category-accessories-test.png';
+      imgSrc = '/gadgets/img/category-accessories.webp';
       bgColor = '#973D5F';
       break;
     default:
-      imgSrc = '/public/gadgets/img/category-phones-test.png';
+      imgSrc = '/public/gadgets/img/category-phones.webp';
       bgColor = '#6D6474';
   }
   return (
-    <div className="flex flex-col gap-6 w-full max-w-[368px]">
+    <div className="group flex flex-col gap-6 w-full max-w-[368px]">
       <div
-        className="w-full h-[288px] flex justify-end items-end overflow-hidden transition-all duration-300 ease-in-out md:h-[187px] lg:h-[368px]"
+        className="block w-full aspect-square overflow-hidden mb-6
+                   transition-transform duration-300 ease-in-out
+                   hover:[&>img]:scale-[0.9] hover:[&>img]:translate-x-0 hover:[&>img]:translate-y-0"
         style={{ backgroundColor: bgColor }}
       >
         <img
           src={imgSrc}
           alt={title}
-          className="w-full h-full object-contain object-right-bottom relative"
+          className="w-full h-full object-contain
+                   transform translate-x-[15%] translate-y-[20%] scale-[1.23]
+                   transition-all duration-300 ease-in-out
+                   group-hover:scale-[0.9] group-hover:translate-x-0 group-hover:translate-y-0"
         />
       </div>
       <div className="flex flex-col items-start">
-        <span className="font-[Mont-SemiBold] text-[20px] text-[#F1F2F9]">
+        <h4 className="text-[20px] font-[Mont-SemiBold] text-[#F1F2F9] leading-[26px] mb-1">
           {title}
-        </span>
-        <span className="count font-[Mont-Regular] text-[14px] leading-[21px] text-gray-400">
+        </h4>
+        <span className="font-[Mont-Regular] text-[14px] leading-[21px] text-gray-400">
           {modelsCount} models
         </span>
       </div>
