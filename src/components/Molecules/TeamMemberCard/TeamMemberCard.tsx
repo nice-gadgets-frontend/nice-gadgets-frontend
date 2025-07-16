e4c4fce054d375de24700d4ce60d53ae2fddb68d
Yaroslav <katsarskyiy@gmail.com>
2025-07-16 13:43:29 +0300

    team page, dark css

--- src/components/Molecules/TeamMemberCard/TeamMemberCard.tsx
+++ src/components/Molecules/TeamMemberCard/TeamMemberCard.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import type { TeamMemberType } from '../../../types/TeamMemberType';
 import { Link } from 'react-router-dom';
+import { Linkedin } from 'lucide-react';
 
 type TeamMemberCardProps = {
   member: TeamMemberType;
@@ -19,10 +20,10 @@ export const TeamMembercard = ({ member, index }: TeamMemberCardProps) => {
   };
 
   return (
-    <Link to={member.linkedIn}>
+    <div>
       <div
         key={member.name}
-        className="relative w-full h-80 cursor-pointer"
+        className="relative w-full h-90"
         style={{ perspective: '1000px' }}
         onMouseEnter={() => handleMouseEnter(index)}
         onMouseLeave={handleMouseLeave}
@@ -44,20 +45,20 @@ export const TeamMembercard = ({ member, index }: TeamMemberCardProps) => {
               height: '100%',
               backfaceVisibility: 'hidden',
             }}
-            className="bg-[#20233d] shadow-xl rounded-lg p-6 flex flex-col items-center text-center min-w-[285px]"
+            className="bg-card shadow-xl rounded-lg p-6 flex flex-col items-center text-center min-w-[285px]"
           >
             <img
               src={member.image}
               alt={member.name}
               className="w-36 h-36 rounded-full object-cover mb-6 border-4 border-[#4CAF50]"
             />
-            <h3 className="text-2xl font-bold mb-2 text-[#f1f2f9]">
+            <h3 className="text-2xl font-bold mb-2 text-primary">
               {member.name}
             </h3>
-            <p className="text-lg font-medium text-[#f1f2f9] mb-2">
+            <p className="text-lg font-medium text-primary mb-2">
               {member.role}
             </p>
-            <p className="text-[#cdced2] leading-relaxed italic">
+            <p className="text-primary leading-relaxed italic">
               {member.shortDescription}
             </p>
           </div>
@@ -70,20 +71,27 @@ export const TeamMembercard = ({ member, index }: TeamMemberCardProps) => {
               backfaceVisibility: 'hidden',
               transform: 'rotateY(180deg)',
             }}
-            className="bg-[#20233d] shadow-xl rounded-lg p-6 flex flex-col items-center justify-center text-center"
+            className="bg-card shadow-xl rounded-lg p-6 flex flex-col items-center justify-center text-center"
           >
-            <h3 className="text-2xl font-bold mb-2 text-[#f1f2f9]">
+            <h3 className="text-2xl font-bold mb-2 text-primary">
               {member.name}
             </h3>
-            <p className="text-lg font-medium text-[#f1f2f9] mb-4">
+            <p className="text-lg font-medium text-primary mb-4">
               {member.role}
             </p>
-            <p className="text-[#cdced2] leading-relaxed text-sm overflow-auto max-h-40 no-scrollbar">
+            <p className="text-primary leading-relaxed text-sm overflow-auto max-h-40 no-scrollbar mb-4">
               {member.longDescription}
             </p>
+            <Link
+              to={member.linkedIn}
+              target="__blank"
+              className="text-[#f1f2f9] p-2 rounded-2xl bg-blue-500 place-self-end"
+            >
+              <Linkedin />
+            </Link>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 };
